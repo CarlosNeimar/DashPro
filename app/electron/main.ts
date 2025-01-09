76a5f32e3a4fa757cdbbddb9db5069b5223309f7
Carlos Henrique Neimar <carlos.areas@sga.pucminas.br>
2025-01-08 23:51:50 -0300

    Add typescript to electron and start config to build the app

--- app/electron/main.ts
+++ app/electron/main.ts
@@ -1,7 +1,9 @@
 import { app, BrowserWindow } from "electron";
 import path from 'path';
 
+type test = string;
+
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({});
-  mainWindow.loadFile(path.join(app.getAppPath(), '../frontend/dist/index.html'));
+  mainWindow.loadFile(path.join(app.getAppPath(), '../frontend/dist-react/index.html'));
 });
--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist-react",
+  },
   base:"./",
   resolve: {
     alias: {
